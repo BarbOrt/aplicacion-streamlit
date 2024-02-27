644bfe92c424570dcc302356c70f0de106b234b2
BarbOrt <barbarabbq@hotmail.com>
2024-02-27 17:47:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,29 @@ import scipy.stats
 import streamlit as st
 import time
 
-# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
-if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
-
-if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+# Inicialización de variables de estado usando operadores ternarios
+st.session_state['experiment_no'] = st.session_state.get('experiment_no', 0)
+st.session_state['df_experiment_results'] = st.session_state.get('df_experiment_results', pd.DataFrame(columns=['no', 'iteraciones', 'media']))
 
 st.header('Lanzar una moneda')
 
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
-
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
     mean = None
     outcome_no = 0
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
-        time.sleep(0.05)
+
+    # Agregar todas las filas al gráfico después de calcularlas
+    chart.add_rows([mean])
+    time.sleep(0.05)
 
     return mean
 
@@ -44,10 +41,10 @@ if start_button:
         pd.DataFrame(data=[[st.session_state['experiment_no'],
                             number_of_trials,
                             mean]],
-                     columns=['no', 'iterations', 'mean'])
+                     columns=['no', 'iteraciones', 'mean'])
         ],
         axis=0)
-    st.session_state['df_experiment_results'] = \\
-        st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
+
